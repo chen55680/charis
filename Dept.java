f844b9ca7fbeff51494e7f3d6b2c95ecc22e75f2
T2000885 <yymm55680@gmail.com>
2020-09-26 16:25:33 +0800

    ver1

---
+++ Dept.java
@@ -0,0 +1,48 @@
+package orm;
+
+import java.io.Serializable;
+
+//此類別對應到資料庫表格
+public class Dept implements Serializable{
+	
+	//這些實體變數對應到部門的表格的各個欄位
+	private int deptno;
+	private String dname;
+	private String loc;
+	//123
+	public Dept() {
+		
+	}
+	
+	public Dept(int deptno,String dname,String loc) {
+		this.deptno=deptno;
+		this.dname=dname;
+		this.loc=loc;
+	}
+
+	public int getDeptno() {
+		return deptno;
+	}
+
+	public void setDeptno(int deptno) {
+		this.deptno = deptno;
+	}
+
+	public String getDname() {
+		return dname;
+	}
+
+	public void setDname(String dname) {
+		this.dname = dname;
+	}
+
+	public String getLoc() {
+		return loc;
+	}
+
+	public void setLoc(String loc) {
+		this.loc = loc;
+	}
+
+
+}
